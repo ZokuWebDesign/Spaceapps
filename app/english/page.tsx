b701efbe84c0bc6fbe1d320e9b9d63ce380c14ac
Juan Pablo <jaypy.uxdesign@gmail.com>
2025-08-19 12:01:54 -0300

    initial setup

--- app/english/page.tsx
+++ app/english/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/portugues/Hero";
+import Hero from "@/components/english/Hero";
 
 export default function Home() {
   return <Hero />;
--- app/espanol/page.tsx
+++ app/espanol/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/portugues/Hero";
+import Hero from "@/components/espanol/Hero";
 
 export default function Home() {
   return <Hero />;
